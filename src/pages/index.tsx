650c9deae0caac43898a8067f175aa5cfb9f93b2
thomas/NERO <tomconmo@gmail.com>
2019-05-28 00:04:43 -0400

    update borders and add embedded player

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,11 @@ const IndexPage = () => (
   </header>
    <div className="page">
       <h2>Coming June 2019</h2>
+      <iframe
+         title="Thomas Tells a Story" id="multi_iframe"
+         src="https://www.podbean.com/media/player/multi?playlist=http%3A%2F%2Fplaylist.podbean.com%2F5158479%2Fplaylist_multi.xml&vjs=1&kdsowie31j4k1jlf913=372203c227412f100e43ef6448879c6f1b0a3716&size=430&skin=10&episode_list_bg=%23ffffff&bg_left=%23000000&bg_mid=%23470909&bg_right=%232a1844&auto=0&download=1&show_playlist_recent_number=10&pbad=1"
+         scrolling="no" width="100%" height="430">
+      </iframe>
    </div>
   </Layout>
 )
