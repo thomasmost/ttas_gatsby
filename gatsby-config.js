cdc4f53374f944d29c84c04ce0ee53259203e63d
thomas/NERO <tomconmo@gmail.com>
2019-06-25 20:02:47 -0400

    change theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,8 +37,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#000`,
+        theme_color: `#000`,
         display: `minimal-ui`,
         icon: `src/ico/favicon-96x96.png`, // This path is relative to the root of the site.
       },
