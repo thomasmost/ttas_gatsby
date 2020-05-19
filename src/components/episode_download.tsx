84fc0f88e3bb3625309d8fe1fc0904be7d53d500
tcm_NIMBUS <tomconmo@gmail.com>
2020-05-19 16:19:54 -0400

    :art: finish new download page

--- src/components/episode_download.tsx
+++ src/components/episode_download.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 import style from "./episode_download.module.scss";
 
-function goTo(link: string) {
-  location.href = link;
-}
-
-const EpisodeDownload = ({ image, link, title }) => (
-  <a className={style.root} onClick={() => goTo(link)}>
-    <div className={style.imgContainer}>
-      <img src={image} className={style.img} />
-    </div>
-    <div className={style.title}>{title}</div>
+const EpisodeDownload = ({ image, link, title, artist, artistLink }) => (
+  <div className={style.root}>
+    <a href={link}>
+      <div className={style.imgContainer}>
+        <img src={image} className={style.img} />
+      </div>
+      <div className={style.title}>{title}</div>
+    </a>
+    <div className={style.artist}>Cover art by <a href={artistLink}>{artist}</a></div>
     <div className={style.clearfix} />
-  </a>
+  </div>
 );
 
 export default EpisodeDownload;
--- src/pages/download.tsx
+++ src/pages/download.tsx
@@ -5,6 +5,8 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 import Navigation from "../components/navigation";
 import EpisodeDownload from "../components/episode_download";
+import PaypalIcon from "../images/paypal.png";
+
 
 // import { useStaticQuery, graphql } from "gatsby";
 
@@ -24,7 +26,18 @@ const DonatePage = () => {
   // console.log(JSON.stringify(data));
   // const coverE01 = data.cloudinaryMedia.secure_url;
 
-  const episodes=[];
+  const episodes=[
+    (
+      <EpisodeDownload
+        key={1}
+        title="Episode 1: Promises"
+        image="https://res.cloudinary.com/thomas-moore/image/upload/v1589909540/gatsby-source-ttas/TheElandrid_S1E01_yeryui.png"
+        link="https://filedn.com/lLL0lkOxLkGHJoHpDELDETb/Elandrid/TheElandrid_S1E01.pdf"
+        artist="Jabari Weathers"
+        artistLink="http://jmwillustration.com/"
+        />
+    )
+  ];
   // const episodes = data.allCloudinaryMedia.edges.map((img, i) => (
   //   console.log(img.node.tags) &&
   //   <EpisodeDownload
@@ -52,26 +65,17 @@ const DonatePage = () => {
         <div className="page-contents">
           <h2>Download</h2>
           <p>
-            Thank you for supporting our work by being a listener! Without you,
-            this podcast would be altogether without purpose.
-          </p>
-          <p>
-            If you would like to go above and beyond to support the show, you
-            can leave a rating or a review, share it with your friends, and
-            engage with us on Twitter or Reddit!
-          </p>
-          <p>
             If you're more of a reader, happen to be hearing-impaired, or simply
             want to immerse yourself in the story through a different medium, we
-            are releasing the episodes as PDFs as well! We ask that you pay what
+            have started releasing the episodes as PDFs as well! We ask that you pay what
             you can to support commissioning cover art and producing more
             episodes of the podcast.
           </p>
           <p>
-            You can to the project by visiting
+            You can download the latest episode texts with the latest (amazing!) original art below. And you can donate to the project by visiting our
             <a href="https://www.buymeacoffee.com/thomastells">
               {" "}
-              buymeacoffee.com/thomastells
+              BMC
             </a>
             , or Paypal us directly at
             <a href="https://www.paypal.me/thomastells">
@@ -80,6 +84,17 @@ const DonatePage = () => {
             </a>
             . Thanks!
           </p>
+          <div className="badges">
+            <div className="bmc" dangerouslySetInnerHTML={{__html: `
+              <style>.bmc-button img{height: 34px !important;width: 35px !important;margin-bottom: 1px !important;box-shadow: none !important;border: none !important;vertical-align: middle !important;}.bmc-button{padding: 7px 15px 7px 10px !important;line-height: 35px !important;height:51px !important;text-decoration: none !important;display:inline-flex !important;color:#ffffff !important;background-color:#5F7FFF !important;border-radius: 5px !important;border: 1px solid transparent !important;padding: 7px 15px 7px 10px !important;font-size: 22px !important;letter-spacing: 0.6px !important;box-shadow: 0px 1px 2px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;margin: 0 auto !important;font-family:'Cookie', cursive !important;-webkit-box-sizing: border-box !important;box-sizing: border-box !important;}.bmc-button:hover, .bmc-button:active, .bmc-button:focus {-webkit-box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;text-decoration: none !important;box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;opacity: 0.85 !important;color:#ffffff !important;}</style><a class="bmc-button" target="_blank" href="https://www.buymeacoffee.com/thomastells"><img src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg" alt="Buy me a coffee"><span style="margin-left:5px;font-size:28px !important;">Buy me a coffee</span></a>
+              `}} 
+            />
+            <div className="paypal">
+              <a href="https://www.paypal.me/thomastells">
+                <img src={PaypalIcon} />
+              </a>
+            </div>
+          </div>
           <h2>
             <em>The Elandrid</em>
           </h2>
