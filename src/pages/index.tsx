9d3616d5a36cc225829e413a15954501af9b2c95
Thomas Constantine Moore <tm@vividcomms.io>
2020-09-22 12:05:16 -0400

    lol what

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import SEO from "../components/seo";
 
 import Title from "../images/the-oa-oliger-title.svg";
 import Navigation from "../components/navigation";
+import { Link } from "gatsby";
 
 const IndexPage = () => (
   <Layout zenMode={false}>
@@ -19,13 +20,16 @@ const IndexPage = () => (
       </div>
     </header>
     <div className="page">
-      <h2>Season 2 arriving September 22nd...</h2>
+      <h2>New Episodes Released Monthly</h2>
+      <p style={{textAlign: 'center'}}>
+        <Link to="/next">Voting is open for Episode 2!</Link>
+      </p>
       <p>
         Thomas Tells a Story is a long-form narrative podcast
         inspired by ancient traditions of storytelling.
       </p>
       <p>
-        Season 2 will be a claustrophobic fantasy, following five adventurers on a quest to find the lost gods of their world:
+        Season 2 is a claustrophobic fantasy, following five adventurers on a quest to find the lost gods of their world:
         Gemujin, an itinerant preacher, mountaineer, and a True Believer...
         Katu, a clerical field researcher and skeptic...
         Narrasset, a Thar warrior and scout sent to accompany the party from the nearest city...
