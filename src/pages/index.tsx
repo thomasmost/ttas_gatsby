d27576723dcce8f31cb6f478ee2258956cb9ac0e
Thomas Constantine Moore <tm@vividcomms.io>
2020-09-30 10:21:06 -0400

    remove link from home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import SEO from "../components/seo";
 
 import Title from "../images/the-oa-oliger-title.svg";
 import Navigation from "../components/navigation";
+import { votingOpen } from "../config";
 
 const IndexPage = () => (
   <Layout zenMode={false}>
@@ -21,7 +22,7 @@ const IndexPage = () => (
     <div className="page">
       <h2>New Episodes Released Monthly</h2>
       <p style={{textAlign: 'center'}}>
-        <a href="/next">Voting is open for Episode 2!</a>
+        {votingOpen ? <a href="/next">Voting is open for Episode 2!</a> : null}
       </p>
       <p>
         Thomas Tells a Story is a long-form narrative podcast
