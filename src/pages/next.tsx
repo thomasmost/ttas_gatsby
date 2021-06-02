53d7c23d6dce4f2714e75408a75122035b861408
tcm_NIMBUS <tomconmo@gmail.com>
2021-06-02 03:34:14 -0400

    release episode 5 voting

--- src/pages/next.tsx
+++ src/pages/next.tsx
@@ -27,7 +27,7 @@ const renderVotingForm = () => (
   <div className="page-contents">
   <h2>What happens next?</h2>
   <p>
-    Voting is now open for Episode 4 of <em>The Oa Oliger</em>. Choose carefully... lives may hang in the balance.
+    Voting is now open for Episode 5 of <em>The Oa Oliger</em>. Choose carefully... lives may hang in the balance.
   </p>
   <form name="rmm_episode_4" netlify method="POST" data-netlify="true" style={formStyles} action="/success">
     <input type="hidden" name="rmm_episode_4" value="rmm_episode_4" />
@@ -40,53 +40,53 @@ const renderVotingForm = () => (
       </p>
     </div>
     <div className="rmm_question" style={questionStyles}>
-      <label className="rmm_question_prompt" style={promptStyles}>Should Yochi ask Gemujin, Katu, or The Cartographer for help?</label>
+      <label className="rmm_question_prompt" style={promptStyles}>Should Yochi ride for the Lake of Spells, or Anrana's Forge?</label>
       <p>
         <label>
-          <input type="radio" value="Gemujin" name="e4_help"/>
-          Gemujin
+          <input type="radio" value="Lake of Spells" name="e5_destination"/>
+          The Lake of Spells
         </label>
       </p>
       <p>
         <label>
-          <input type="radio" value="Katu" name="e4_help"/>
-          Katu
+          <input type="radio" value="Anrana's Forge" name="e5_destination"/>
+          Anrana's Forge
+        </label>
+      </p>
+    </div>
+    <div className="rmm_question" style={questionStyles}>
+      <label className="rmm_question_prompt" style={promptStyles}>Will Yochi leave a clue for Gemujin or for Katu?</label>
+      <p>
+        <label>
+          <input type="radio" value="Gemujin" name="e5_clue"/>
+          Yes, to Gemujin
         </label>
       </p>
       <p>
         <label>
-          <input type="radio" value="The Cartographer" name="e4_help"/>
+          <input type="radio" value="Katu" name="e5_clue"/>
+          Yes, to Katu
+        </label>
+      </p>
+      <p>
+        <label>
+          <input type="radio" value="No one" name="e5_clue"/>
+          No. No one.
+        </label>
+      </p>
+    </div>
+    <div className="rmm_question" style={questionStyles}>
+      <label className="rmm_question_prompt" style={promptStyles}>Should Gemujin command the Cartographer or Katu to stay at the castle with Narrasset?</label>
+      <p>
+        <label>
+          <input type="radio" value="The Cartographer" name="e5_gemujin_command"/>
           The Cartographer
         </label>
       </p>
-    </div>
-    <div className="rmm_question" style={questionStyles}>
-      <label className="rmm_question_prompt" style={promptStyles}>Should Yochi tell everyone about the Red Mouthed Man, or just someone he trusts?</label>
       <p>
         <label>
-          <input type="radio" value="Everyone" name="e4_tell"/>
-          Everyone
-        </label>
-      </p>
-      <p>
-        <label>
-          <input type="radio" value="Someone he trusts" name="e4_tell"/>
-          Someone he trusts
-        </label>
-      </p>
-    </div>
-    <div className="rmm_question" style={questionStyles}>
-      <label className="rmm_question_prompt" style={promptStyles}>What should Yochi do with the strange rock he finds?</label>
-      <p>
-        <label>
-          <input type="radio" value="Keep" name="e4_strange_rock"/>
-          Keep it for himself
-        </label>
-      </p>
-      <p>
-        <label>
-          <input type="radio" value="Keep" name="e4_strange_rock"/>
-          Keep it for himself
+          <input type="radio" value="Katu" name="e5_gemujin_command"/>
+          Katu
         </label>
       </p>
     </div>
@@ -101,7 +101,7 @@ const renderVotingClosed = () => (
   <div className="page-contents">
   <h2>What happens next?</h2>
   <p>
-    Voting is now closed for Episode 4 of <em>The Oa Oliger</em>.
+    Voting is now closed for Episode 5 of <em>The Oa Oliger</em>.
   </p>
   <p>
     The light still shines. The wind still blows.
